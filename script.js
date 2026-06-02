52c8542c71148c43a4d307294f8dbce24aee33c5
adriangregoluz-ctrl <adrian.grego.luz@escola.pr.gov.br>
2026-06-02 14:12:19 -0300

    Add function to display a message in HTML

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "A tecnologia permite produzir mais alimentos, economizar recursos naturais e melhorar a qualidade de vida no campo.";
+}
